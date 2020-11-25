755990e855eaa95f93008fa40177b1daba7aa1a7
Hitesh Sahu <hd-hitesh@users.noreply.github.com>
2020-11-26 00:09:20 +0530

    x

---
+++ 34.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define len(x) x.size()
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define FOR(i,j,n) for(int i=j;i<n;i++)
+#define FORR(i,j,n) for(int i=j;i>n;i--)
+// #define FOR(i,n) for(int i=0;i<n;i++)
+// #define FORR(i,n) for(int i=j;i>=0;i--)
+#define all(v) v.begin(), v.end()
+#define endl "\n";
+#define test(n) cout<<"___"<<n<<"___\n";
+#define tez_chal_bsdk                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef unordered_map<int, int> mp;
+typedef unordered_map<ll, ll> mpl;
+
+#define cint(a) int a; cin>>a;
+#define cin(a) ll a; cin>>a;
+#define cin2(a,b) ll a,b; cin>>a>>b;
+#define cin3(a,b,c) ll a,b,c; cin>>a>>b>>c;
+#define vin(v,n) for(ll i=0; i<n;i++) cin>>v[i];
+#define vout(v,n) for(ll i=0; i<n;i++) cout<<v[i]<<" "; cout<<endl;
+
+vll pr;
+void SieveOfEratosthenes()
+{
+    ull n = 2000000;;
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (ull p = 2; p * p <= n; p++)
+    {
+        if (prime[p] == true)
+        {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    // Print all prime numbers
+    for (ull p = 2; p <= n; p++)
+        if (prime[p])
+        {
+            pr.push_back(p);
+            // cout << p << " ";
+        }
+    ull q = 0;
+    n = pr.size();
+     // test(n);
+    // while(q<20){
+    //     cout<<pr[q]<<" ";q++;
+    //     // q=q+100;
+    // }
+    // cout<<endl;
+}
+
+void solve()
+{
+    cin(num);
+    ull aa = lower_bound(all(pr),num)-pr.begin();
+    // ull b = upper_bound(all(pr),num)-pr.begin();
+    // cout<<a<<" "<<b<<endl;
+
+    // ull a = pr[aa];
+    // ull b = pr[aa-1];
+    // cout<<a<<" "<<b<<endl;
+
+    if (pr[pr[aa]]==num)
+    {
+        cout<<num;
+    }else if(num-pr[aa-1] <= pr[aa]-num)
+    cout<<pr[aa-1];
+    else
+        cout<<pr[aa];;
+}
+
+int main()
+{
+    tez_chal_bsdk;
+#ifndef ONLINE_JUDGE
+    freopen("input1.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
+#endif
+    SieveOfEratosthenes();
+
+    // solve();
+    cin(t);
+    while (t--)
+    {
+        solve();
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// ctrl +  alt + F   :  format the code
+// ctrl + shift + B  :  build with
+// ctrl + B          :  build
+// ctrl + X          :  cut the line
+// ctrl +
